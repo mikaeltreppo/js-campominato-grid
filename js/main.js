a8dff6767efdce4a4f5770e6da816a64acc87a2f
mikaeltreppo <mikaeltreppo@outlook.com>
2023-03-04 08:57:28 +0100

    aggiornato con tutto

--- js/main.js
+++ js/main.js
@@ -1,16 +1,39 @@
 let entranceDom = document.getElementById('entrance');
 
-for (let i = 1; i <= 100; i++)
-{
-    let boxCreator = createBox();
-    entranceDom.append (boxCreator);
+
+
+
+let buttonDom = document.getElementById('buttonStartGame');
+//riparte il gioco
+buttonDom.addEventListener('click',
+    function () {
+        entranceDom.classList.toggle('d-none')
+    });
+
+
+
+
+
+
+
+//creato numero all'interno e box
+for (let i = 1; i <= 100; i++) {
+    let boxCreated = createBox();
+    let num = i;
+    entranceDom.append(boxCreated);
+    boxCreated.innerHTML = num;
+    boxCreated.addEventListener('click',
+        function () {
+            console.log(num)
+        })
 }
 
 
 
 
 
-function createBox () {
+//funzione creare caselle
+function createBox() {
     let newBox = document.createElement('div')
     newBox.classList.add('boxFinished');
     return newBox;
